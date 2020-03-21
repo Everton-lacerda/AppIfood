fc65c108b999548c6524d1da9f5b922a9da82868
Everton Lacerda <everton3b@hotmail.com>
2020-03-21 16:58:52 -0300

    add routes children for components parents

---
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reviews',
+  templateUrl: './reviews.component.html',
+  styleUrls: ['./reviews.component.sass']
+})
+export class ReviewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
